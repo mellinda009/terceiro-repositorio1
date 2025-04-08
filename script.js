213ab6e8e46e95b69023946b755b94b4f353efe3
mellinda009 <132473744+mellinda009@users.noreply.github.com>
2025-04-07 22:52:42 -0300

    aula 07/04

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-document.addEventListener(‘DOMContentLoaded’, function(){
-    const aumentaFonteBotao = document.getElementById(‘aumentarfonte’);
-    aumentaFonteBotao.addEventListener(‘click’, function(){ })
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentarfonte');
+    aumentaFonteBotao.addEventListener('click', function(){ 
+
     })
+const botaoDeAssesibilidade = document.getElementById ('botao-aceessibilidade');
+const opcoesDeAcessibilidade = document.getElementById ('opcoes-acessibilidade');
+
+ })
